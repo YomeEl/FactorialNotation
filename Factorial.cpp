55b9bf54ba2ced9416f7d264fec4f8b286c18a3a
Ярослав <umpepatopChannel@gmail.com>
2020-12-04 01:39:50 +0500

    Fixed string representation

--- Factorial.cpp
+++ Factorial.cpp
@@ -23,17 +23,38 @@ std::string Factorial::AsSum()
 	std::string result = "";
 	for (int i = 0; i < FACTORIALS_COUNT; i++)
 	{
-		result += std::to_string(fac[i]) + " * " + std::to_string(i) + "! + ";
+		if (fac[i] > 0)
+		{
+			result += std::to_string(fac[i]) + " * " + std::to_string(i) + "! + ";
+		}
 	}
-	result.erase(result.length() - 3, 3);
+	if (result.length() > 0)
+	{
+		result.erase(result.length() - 3, 3);
+	}
+	else
+	{
+		result = "0";
+	}
 
 	return result;
 }
 
 std::string Factorial::AsVector()
 {
+	int cnt = FACTORIALS_COUNT;
+	while (fac[cnt] == 0)
+	{
+		cnt--;
+	}
+
+	if (cnt == 0)
+	{
+		return "(0)";
+	}
+
 	std::string result = "(";
-	for (int i = 0; i < FACTORIALS_COUNT; i++)
+	for (int i = 0; i < cnt; i++)
 	{
 		result += std::to_string(fac[i]) + ", ";
 	}
